5d5f9fa53b811b6644fc5f23758b1d114d14124d
er1cyao <104165512+er1cyao@users.noreply.github.com>
2025-09-09 15:01:00 -0400

    Adding R and Dependencies

---
+++ install.R
@@ -0,0 +1,6 @@
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("rmarkdown", dependencies = TRUE)
+install.packages("IRkernel", dependencies = TRUE)
+
+# Install IRkernel for Jupyter
+IRkernel::installspec(user = FALSE)
